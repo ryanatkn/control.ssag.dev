9ff039cb2f0bfa8a20faf3db930423633601cdc7
Ryan Atkinson <mail@ryanatkn.com>
2023-02-26 06:31:31 -0700

    more

--- src/routes/ItemDetails.svelte
+++ src/routes/ItemDetails.svelte
@@ -2,33 +2,80 @@
 	import type {Item} from '@feltcoop/dealt';
 	import type {Writable} from 'svelte/store';
 
-	import {toHslCss} from '$lib/util';
-
 	export let item: Writable<Item>;
 	export let item_selection: Writable<Writable<Item> | null>;
 
 	$: selected_item = $item_selection;
 
+	$: console.log(`$selected_item`, $selected_item);
+
 	// TODO BLOCK destroy is bugged, do we need `stage.destroy_item`? or `item.stage`?
 </script>
 
 {#if $selected_item}
-	<div>
-		<label style:--text_color={toHslCss($item.color)}>
-			<div class="title">type</div>
-			<select value={$selected_item.type}>
-				<option value="circle">circle</option>
-				<option value="polygon">polygon</option>
-			</select>
-		</label>
-		<label>
-			<div class="title">id</div>
-			<input value={$selected_item.id} />
-		</label>
-		<label>
-			<div class="title">text</div>
-			<input value={$selected_item.text} /> (TODO bind me)
-		</label>
-	</div>
-	<button on:click={() => $item.destroy()}>delete {$selected_item.type}</button>
+	<form>
+		(TODO bind these)
+		<div>
+			<fieldset>
+				<label>
+					<div class="title">type</div>
+					<select value={$selected_item.type}>
+						<option value="circle">circle</option>
+						<option value="polygon">polygon</option>
+					</select>
+				</label>
+			</fieldset>
+			<fieldset class="row">
+				<label>
+					<div class="title">x</div>
+					<input type="x" value={$selected_item.x} />
+				</label>
+				<label>
+					<div class="title">y</div>
+					<input type="y" value={$selected_item.y} />
+				</label>
+			</fieldset>
+			<fieldset>
+				<label>
+					<div class="title">id</div>
+					<input value={$selected_item.id} />
+				</label>
+			</fieldset>
+			<fieldset>
+				<label>
+					<div class="title">graphicsLineColor</div>
+					<input value={$selected_item.graphicsLineColor} />
+				</label>
+			</fieldset>
+			<fieldset>
+				<label>
+					<div class="title">graphicsLineAlpha</div>
+					<input
+						type="range"
+						min={0}
+						max={1}
+						step={0.01}
+						value={$selected_item.graphicsLineAlpha}
+					/>
+				</label>
+			</fieldset>
+			<fieldset class="row">
+				<label>
+					<div class="title">text</div>
+					<input value={$selected_item.text} />
+				</label>
+				<label>
+					<div class="title">color</div>
+					<input type="color" value={$selected_item.color} />
+				</label>
+			</fieldset>
+		</div>
+		<button on:click={() => $item.destroy()}>delete {$selected_item.type}</button>
+	</form>
 {/if}
+
+<style>
+	fieldset {
+		margin-bottom: var(--spacing_md);
+	}
+</style>
