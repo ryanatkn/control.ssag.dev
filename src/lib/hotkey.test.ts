87db581a31f9c351d0ae368be9aa9326ec154090
Ryan Atkinson <mail@ryanatkn.com>
2023-03-05 13:51:55 -0700

    fix test description

--- src/lib/hotkey.test.ts
+++ src/lib/hotkey.test.ts
@@ -8,7 +8,7 @@ test('matches a simple pattern', () => {
 	assert.ok(matchesHotkey({match: 'a', action: noop}, 'a'));
 });
 
-test('matches comma-separated values', () => {
+test('matches multiple values', () => {
 	assert.ok(matchesHotkey({match: ['a', 'b'], action: noop}, 'a'));
 	assert.ok(matchesHotkey({match: ['a', 'b'], action: noop}, 'b'));
 	assert.ok(matchesHotkey({match: ['a', 'b', 'c'], action: noop}, 'a'));
