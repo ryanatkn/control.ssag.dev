511ab11fcca63cb06ac956f4286c022eeaad564f
Ryan Atkinson <mail@ryanatkn.com>
2023-03-05 13:37:33 -0700

    add tests

---
+++ src/lib/hotkey.test.ts
@@ -0,0 +1,25 @@
+import {test} from 'uvu';
+import * as assert from 'uvu/assert';
+
+import {matchesHotkey, type Hotkey} from '$lib/hotkey';
+import {noop} from '@feltjs/util';
+
+test('matches a simple pattern', () => {
+	matchesHotkey({match: 'a', action: noop}, 'a');
+});
+
+test('matches comma-separated values', () => {
+	matchesHotkey({match: 'a,b', action: noop}, 'a');
+	matchesHotkey({match: 'a,b', action: noop}, 'b');
+	matchesHotkey({match: 'a,b,c', action: noop}, 'a');
+	matchesHotkey({match: 'a,b,c', action: noop}, 'b');
+	matchesHotkey({match: 'a,b,c', action: noop}, 'c');
+});
+
+test('caches the parsed data', () => {
+	const hotkey: Hotkey = {match: 'a', action: noop};
+	matchesHotkey(hotkey, 'a');
+	assert.ok(hotkey.parsed);
+});
+
+test.run();
