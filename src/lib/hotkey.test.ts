97430b6c487a742bc2a03e24da7d9af45ea9cecd
Ryan Atkinson <mail@ryanatkn.com>
2023-03-05 13:48:08 -0700

    refactor hotkey

--- src/lib/hotkey.test.ts
+++ src/lib/hotkey.test.ts
@@ -2,24 +2,18 @@ import {test} from 'uvu';
 import * as assert from 'uvu/assert';
 import {noop} from '@feltjs/util/function.js';
 
-import {matchesHotkey, type Hotkey} from '$lib/hotkey';
+import {matchesHotkey} from '$lib/hotkey';
 
 test('matches a simple pattern', () => {
-	matchesHotkey({match: 'a', action: noop}, 'a');
+	assert.ok(matchesHotkey({match: 'a', action: noop}, 'a'));
 });
 
 test('matches comma-separated values', () => {
-	matchesHotkey({match: 'a,b', action: noop}, 'a');
-	matchesHotkey({match: 'a,b', action: noop}, 'b');
-	matchesHotkey({match: 'a,b,c', action: noop}, 'a');
-	matchesHotkey({match: 'a,b,c', action: noop}, 'b');
-	matchesHotkey({match: 'a,b,c', action: noop}, 'c');
-});
-
-test('caches the parsed data', () => {
-	const hotkey: Hotkey = {match: 'a', action: noop};
-	matchesHotkey(hotkey, 'a');
-	assert.ok(hotkey.parsed);
+	assert.ok(matchesHotkey({match: ['a', 'b'], action: noop}, 'a'));
+	assert.ok(matchesHotkey({match: ['a', 'b'], action: noop}, 'b'));
+	assert.ok(matchesHotkey({match: ['a', 'b', 'c'], action: noop}, 'a'));
+	assert.ok(matchesHotkey({match: ['a', 'b', 'c'], action: noop}, 'b'));
+	assert.ok(matchesHotkey({match: ['a', 'b', 'c'], action: noop}, 'c'));
 });
 
 test.run();
--- src/lib/hotkey.ts
+++ src/lib/hotkey.ts
@@ -1,11 +1,10 @@
 // TODO improve, maybe `shift+a,b,ctrl+shift+c`
 export interface Hotkey {
-	match: string;
+	match: HotkeyMatch | HotkeyMatch[];
 	action: () => void;
-	parsed?: ParsedHotkeyMatch[];
 }
 
-export type ParsedHotkeyMatch = string;
+export type HotkeyMatch = string; // TODO add support for shift, press duration, ...
 
 /**
  * Returns `true` if `key` matches the `hotkey`.
@@ -15,7 +14,7 @@ export type ParsedHotkeyMatch = string;
  * @returns
  */
 export const matchesHotkey = (hotkey: Hotkey, key: string): boolean => {
-	const parsed = parseHotkeyMatch(hotkey);
+	const parsed = parseHotkeyMatches(hotkey);
 	for (const p of parsed) {
 		if (p === key) {
 			return true;
@@ -24,5 +23,7 @@ export const matchesHotkey = (hotkey: Hotkey, key: string): boolean => {
 	return false;
 };
 
-const parseHotkeyMatch = (hotkey: Hotkey): ParsedHotkeyMatch[] =>
-	hotkey.parsed || (hotkey.parsed = hotkey.match.split(','));
+const parseHotkeyMatches = (hotkey: Hotkey): HotkeyMatch[] => {
+	const {match} = hotkey;
+	return typeof match === 'string' ? [match] : match;
+};
