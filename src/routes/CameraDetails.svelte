0b7afcc3a7c59b9fb79b9779413e3443a9351e7e
Ryan Atkinson <mail@ryanatkn.com>
2023-03-07 23:46:15 -0700

    todo

--- src/routes/CameraDetails.svelte
+++ src/routes/CameraDetails.svelte
@@ -11,6 +11,8 @@
 	// TODO type is awkward
 	const updateX = (e: Event & {target: any}) => camera.setPosition(Number(e.target.value), y);
 	const updateY = (e: Event & {target: any}) => camera.setPosition(x, Number(e.target.value));
+	// TODO upstream `setScale`
+	// const updateScale = (e: Event & {target: any}) => camera.setScale(Number(e.target.value));
 </script>
 
 <div class="camera-details">
@@ -34,6 +36,14 @@
 					<input type="range" value={y} on:input={updateY} min={0} max={WORLD_SIZE} />
 				</label>
 			</fieldset>
+			<!-- TODO upstream `setScale` -->
+			<!-- <fieldset>
+				<label>
+					<span class="title">scale</span>
+					<input type="number" value={scale} on:input={updateScale} />
+					<input type="range" value={scale} on:input={updateScale} min={0} max={10} />
+				</label>
+			</fieldset> -->
 		</div>
 	</form>
 </div>
