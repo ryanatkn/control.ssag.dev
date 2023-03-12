0411759ff603b67e08aaf5911b1c7d46210cba71
Ryan Atkinson <mail@ryanatkn.com>
2023-03-11 18:57:46 -0700

    wip

---
+++ src/routes/ItemControls.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import type {Item} from '@feltcoop/dealt';
+
+	export let item: Item;
+</script>
+
+<div class="item-controls">
+	<!-- TODO handles to set item.x/y -->
+</div>
+
+<style>
+	.item-controls {
+		position: absolute;
+		left: 0;
+		top: 0;
+	}
+</style>
